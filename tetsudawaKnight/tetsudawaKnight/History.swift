30d0871b334c320b3b87dedae091e19e62904c4c
e155740 <e155740>
2016-12-22 10:35:01 +0900

    swift

---
+++ tetsudawaKnight/tetsudawaKnight/History.swift
@@ -0,0 +1,26 @@
+//
+//  History.swift
+//  tetsudawaKnight
+//
+//  Created by YUUYA PC on 2016/12/22.
+//  Copyright © 2016年 Kaito Ishizuka. All rights reserved.
+//
+
+import UIKit
+
+class History: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ tetsudawaKnight/tetsudawaKnight/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  tetsudawaKnight
+//
+//  Created by Kaito Ishizuka on 2016/12/15.
+//  Copyright © 2016年 Kaito Ishizuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
