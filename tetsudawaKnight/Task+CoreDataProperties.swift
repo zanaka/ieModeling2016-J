9aecdf00fe043001e02ed096ee4e07e9d701a257
KaitoIshizuka <e155716@ie.u-ryukyu.ac.jp>
2017-02-13 10:53:07 +0900

    pull 前

--- tetsudawaKnight/Task+CoreDataProperties.swift
+++ tetsudawaKnight/Task+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Task+CoreDataProperties.swift
 //  tetsudawaKnight
 //
-//  Created by Kaito Ishizuka on 2017/01/25.
+//  Created by Kaito Ishizuka on 2017/02/13.
 //  Copyright © 2017年 Kaito Ishizuka. All rights reserved.
 //
 
@@ -19,5 +19,6 @@ extension Task {
     @NSManaged public var clearPrice: Int16
     @NSManaged public var id: String?
     @NSManaged public var taskName: String?
+    @NSManaged public var isComp: Bool
 
 }
--- tetsudawaKnight/tetsudawaKnight/Data.swift
+++ tetsudawaKnight/tetsudawaKnight/Data.swift
@@ -15,12 +15,14 @@ struct TaskStruct
     var name :String = "task"
     var yen : Int16 = 100
     var id : String = "id"
+    var isComp : Bool = false
     
-    init(name :String, yen :Int16, id :String)
+    init(name :String, yen :Int16, id :String, isComp :Bool)
     {
         self.name = name
         self.yen = yen
         self.id = id
+        self.isComp = isComp
     }
 }
 
@@ -28,6 +30,12 @@ var taskList: [TaskStruct] =
 [
 
 ]
+
+var HisTaskList: [TaskStruct] =
+[
+
+]
+
 
 var money = 1000
 var payHistoryList = ["100"]
